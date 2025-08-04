09707f1a858ce1be4dfec0cfaea218e8b2043b02
Wect <2602710174@qq.com>
2025-08-04 17:02:07 +0800

    完成

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,13 +1,27 @@
 import "../scss/Favorites.scss"
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../cpmponents/MovieCard";
 
-
-function Favorites () {
-
+function Favorites() {
+  const { favorites } = useMovieContext();
+  console.log(favorites);
+  
+  if (favorites.length !== 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites </h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="favourites-empty">
       <h2>No Favourite Movies Yet</h2>
       <p>Start adding movies to your favourites and they will apear here</p>
-    
     </div>
   )
 }
